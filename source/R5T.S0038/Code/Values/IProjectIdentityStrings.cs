1b00c9cc71314b130598f90c9f4f749f640a455a
davidcoats <david@getsafetycone.com>
2023-07-16 16:25:28 -0600

    Interim changes.

--- source/R5T.S0038/Code/Values/IProjectIdentityStrings.cs
+++ source/R5T.S0038/Code/Values/IProjectIdentityStrings.cs
@@ -10,8 +10,8 @@ namespace R5T.S0038
 	[ValuesMarker]
 	public interface IProjectIdentityStrings : IValuesMarker
 	{
-		public string R5T_T0131 => Instances.ProjectPath.R5T_T0131();
-		public string R5T_T0132 => Instances.ProjectPath.R5T_T0132();
-		public string R5T_T0141 => Instances.ProjectPath.R5T_T0141();
+		public string R5T_T0131 => "";/* Instances.ProjectPath.R5T_T0131();*/
+		public string R5T_T0132 => "";/* Instances.ProjectPath.R5T_T0132();*/
+		public string R5T_T0141 => "";/* Instances.ProjectPath.R5T_T0141();*/
 	}
 }
